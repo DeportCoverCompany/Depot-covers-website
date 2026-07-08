d56ba711bc3253991ee377e01be43fd61ff3ecaf
peterrobert <pwambui93@gmail.com>
2026-07-08 12:53:46 +0300

    Added meta ta

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,19 @@
 import { createRoot } from "react-dom/client";
+import { Helmet } from "react-helmet-async";
 // @ts-ignore: Allow CSS side-effect import in this project setup
 import "./index.css";
 import App from "./App.tsx";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <>
+    <Helmet>
+      <title>Leadforge Studio | Web Development in Kenya</title>
+      <meta
+        name="description"
+        content="Depot Covers Company (DCC) — Branding & Printing Solutions Kenya"
+      />
+    </Helmet>
+
+    <App />
+  </>,
+);
